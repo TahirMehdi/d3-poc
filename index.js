d1d4b35d933c438044cfafb80a1de1406ef34a06
Unknown <tahir.mehdiyev@gmail.com>
2017-02-10 21:37:49 +0300

    bars added

--- index.js
+++ index.js
@@ -1,3 +1,256 @@
-/**
- * Created by tairmehdi on 07.02.17.
- */
+(function () {
+
+    var fakeData = [
+        {
+            title: 'Acromantis',
+            subhead: 'insect',
+            details: '',
+            list: [
+                {
+                    title: 'Australis',
+                    value: 1871,
+                    details: ''
+
+                }, {
+                    title: 'Dyaka',
+                    value: 1920,
+                    details: ''
+                }, {
+                    title: 'Elegans',
+                    value: 1993,
+                    details: ''
+                }, {
+                    title: 'Formosana',
+                    value: 1911,
+                    details: '(Taiwan flower mantis)'
+                }, {
+                    title: 'Gestri',
+                    value: 1915,
+                    details: '(Thai boxer mantis, Sumatran Acromantis)'
+                }
+            ]
+        }, {
+            title: 'Mantis',
+            subhead: 'insect',
+            details: '',
+            list: [
+                {
+                    title: 'Acromantis australis',
+                    value: 1871,
+                    details: ''
+
+                }, {
+                    title: 'Acromantis dyaka',
+                    value: 1920,
+                    details: ''
+                }, {
+                    title: 'Acromantis elegans',
+                    value: 1993,
+                    details: ''
+                }, {
+                    title: 'Acromantis formosana',
+                    value: 1911,
+                    details: '(Taiwan flower mantis)'
+                }, {
+                    title: 'Acromantis gestri',
+                    value: 1915,
+                    details: '(Thai boxer mantis, Sumatran Acromantis)'
+                }
+            ]
+        }, {
+            title: 'Mantis',
+            subhead: 'insect',
+            details: '',
+            list: [
+                {
+                    title: 'Acromantis australis',
+                    value: 1871,
+                    details: ''
+
+                }, {
+                    title: 'Acromantis dyaka',
+                    value: 1920,
+                    details: ''
+                }, {
+                    title: 'Acromantis elegans',
+                    value: 1993,
+                    details: ''
+                }, {
+                    title: 'Acromantis formosana',
+                    value: 1911,
+                    details: '(Taiwan flower mantis)'
+                }, {
+                    title: 'Acromantis gestri',
+                    value: 1915,
+                    details: '(Thai boxer mantis, Sumatran Acromantis)'
+                }
+            ]
+        }, {
+            title: 'Mantis',
+            subhead: 'insect',
+            details: '',
+            list: [
+                {
+                    title: 'Acromantis australis',
+                    value: 1871,
+                    details: ''
+
+                }, {
+                    title: 'Acromantis dyaka',
+                    value: 1920,
+                    details: ''
+                }, {
+                    title: 'Acromantis elegans',
+                    value: 1993,
+                    details: ''
+                }, {
+                    title: 'Acromantis formosana',
+                    value: 1911,
+                    details: '(Taiwan flower mantis)'
+                }, {
+                    title: 'Acromantis gestri',
+                    value: 1915,
+                    details: '(Thai boxer mantis, Sumatran Acromantis)'
+                }
+            ]
+        }, {
+            title: 'Mantis',
+            subhead: 'insect',
+            details: '',
+            list: [
+                {
+                    title: 'Acromantis australis',
+                    value: 1871,
+                    details: ''
+
+                }, {
+                    title: 'Acromantis dyaka',
+                    value: 1920,
+                    details: ''
+                }, {
+                    title: 'Acromantis elegans',
+                    value: 1993,
+                    details: ''
+                }, {
+                    title: 'Acromantis formosana',
+                    value: 1911,
+                    details: '(Taiwan flower mantis)'
+                }, {
+                    title: 'Acromantis gestri',
+                    value: 1915,
+                    details: '(Thai boxer mantis, Sumatran Acromantis)'
+                }
+            ]
+        }, {
+            title: 'Mantis',
+            subhead: 'insect',
+            details: '',
+            list: [
+                {
+                    title: 'Acromantis australis',
+                    value: 1871,
+                    details: ''
+
+                }, {
+                    title: 'Acromantis dyaka',
+                    value: 1920,
+                    details: ''
+                }, {
+                    title: 'Acromantis elegans',
+                    value: 1993,
+                    details: ''
+                }, {
+                    title: 'Acromantis formosana',
+                    value: 1911,
+                    details: '(Taiwan flower mantis)'
+                }, {
+                    title: 'Acromantis gestri',
+                    value: 1915,
+                    details: '(Thai boxer mantis, Sumatran Acromantis)'
+                }
+            ]
+        }, {
+            title: 'Mantis',
+            subhead: 'insect',
+            details: '',
+            list: [
+                {
+                    title: 'Acromantis australis',
+                    value: 1871,
+                    details: ''
+
+                }, {
+                    title: 'Acromantis dyaka',
+                    value: 1920,
+                    details: ''
+                }, {
+                    title: 'Acromantis elegans',
+                    value: 1993,
+                    details: ''
+                }, {
+                    title: 'Acromantis formosana',
+                    value: 1911,
+                    details: '(Taiwan flower mantis)'
+                }, {
+                    title: 'Acromantis gestri',
+                    value: 1915,
+                    details: '(Thai boxer mantis, Sumatran Acromantis)'
+                }
+            ]
+        }
+    ];
+    var N = 20,
+        H = 15,
+        generator = d3.randomUniform(1, H - 2),
+        color = d3.scaleSequential(d3.interpolateViridis).domain([0, H + 5]),
+        // areaColor = color(H + 5),
+        data = d3.range(0, N).map(function () {
+            return generator();
+        });
+
+    var width = (window.innerWidth - 20),
+        height = (window.innerHeight - 20),
+        y = d3.scaleLinear().domain([0, H]).range([height, 0]),
+        barWidth = width / fakeData.length;
+    // var side = 0.5*width;
+
+    var paper = d3.select('#paper')
+        .attr('width', width)
+        .attr('height', height)
+        .canvas(true);
+
+    paper
+        .append('rect')
+        .attr('width', width)
+        .attr('height', height)
+        .style('fill', '#000000');
+
+
+    paper.append('g')
+        .selectAll('rect')
+        .data(data)
+        .enter()
+        .append("rect")
+        .attr("transform", function (d, i) {
+            return "translate(" + i * barWidth + ")";
+        })
+        .attr("y", function (d) {
+            return y(d);
+        })
+        .attr("height", function (d) {
+            return height - y(d);
+        })
+        .attr("width", barWidth - 1)
+        .attr("fill", function (d) {
+            return color(d);
+        })
+        .attr("stroke", '#fff')
+        .attr("stroke-width", 1)
+        .attr("stroke-linejoin", "round");
+
+    // var area = paper
+    //     .append('rect')
+    //     .style("fill", areaColor)
+    //     .style("fill-opacity", 0.6);
+})
+();
